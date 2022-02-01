95f43e2591ac3ca30b69550a3ccb5f9bc5f3620a
Anji6789 <59438036+Anji6789@users.noreply.github.com>
2022-02-01 15:54:06 +0530

    Update CreateWorkItem.ps1

--- Scripts/CreateWorkItem.ps1
+++ Scripts/CreateWorkItem.ps1
@@ -12,7 +12,7 @@ Invoke-RestMethod -Uri $uriAccount -Method get -Headers $AzureDevOpsAuthenicatio
 
 #Create a work item
 
-$WorkItemType = "task"
+$WorkItemType = "bug"
 $WorkItemTitle = "Test from Powershell"
 $ProjectName = "apicall";
 
