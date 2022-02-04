f9eb62ce3a94902bd4359e40e6b62d299acb6778
tarunsharmaji9876 <96136095+tarunsharmaji9876@users.noreply.github.com>
2022-02-04 19:28:13 +0530

    Update CreateWorkItem.ps1

--- Scripts/CreateWorkItem.ps1
+++ Scripts/CreateWorkItem.ps1
@@ -15,7 +15,7 @@ Invoke-RestMethod -Uri $uriAccount -Method get -Headers $AzureDevOpsAuthenicatio
 $WorkItemType = "Task"
 $WorkItemTitle = "Test from Powershell"
 $WorkItemDescription = "Task creation"
-$WorkItemActivity = "Documentation"
+$WorkItemState = "Doing"
 $ProjectName = "test_code";
 
 
